52b25e6971e451a516a9900fdfb54175e97232d7
Sergey <strelnikov-sv@yandex.ru>
2026-01-24 12:27:45 +0300

    v.1.0.3
    Вынести политику сборки в cmake-модули

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,1 +1,4 @@
-# Placeholder for future option-related helpers.
+option(DFH_NODE_BUILD_TESTS "Build tests" ON)
+option(DFH_NODE_BUILD_EXAMPLES "Build examples" ON)
+option(DFH_NODE_USE_SYSTEM_DEPS "Use system dependencies instead of third_party" OFF)
+option(DFH_NODE_ENABLE_TLS "Enable TLS support" OFF)
---
+++ cmake/ThirdParty.cmake
@@ -0,0 +1,5 @@
+# Placeholder for third-party dependencies.
+#
+# Here we will add add_subdirectory(third_party/...) entries.
+# Later we will add a system deps mode via DFH_NODE_USE_SYSTEM_DEPS.
+# Pins and licenses live in docs/third_party.md.
